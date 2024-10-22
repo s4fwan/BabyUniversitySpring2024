7f1940e37f3e155cfd4794a394f0e8023d156fd7
Theo <thienpham@Thiens-MacBook-Air.local>
2024-10-22 20:18:00 +1100

    ok

--- front-end/SignUp/signup.js
+++ front-end/SignUp/signup.js
@@ -1,181 +1,246 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import {BASE_API_URL} from "@env"
-import { Text, TextInput, View, StyleSheet, TouchableOpacity, KeyboardAvoidingView, Image, ScrollView } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import phyDoodleShapes from '../assets/BgImage/doodle.png';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useState } from "react";
+import axios from "axios";
+import { BASE_API_URL } from "@env";
+import {
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Image,
+  ScrollView,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import phyDoodleShapes from "../assets/BgImage/doodle.png";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import intersect from "../assets/img/Intersect.png";
 
 const validateEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 const validateName = (name) => /^[A-Za-z]+$/.test(name);
 const validatePin = (pin) => /^\d{4}$/.test(pin);
 
 const SignupScreen = () => {
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [pin, setPin] = useState('');
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [pin, setPin] = useState("");
   const [errors, setErrors] = useState({});
 
   const navigation = useNavigation();
-  
-const handleSignup = async () => {
-  const newErrors = {};
 
-  if (!email) newErrors.email = 'Please enter your email.';
-  else if (!validateEmail(email)) newErrors.email = 'Invalid email format.';
+  const handleSignup = async () => {
+    const newErrors = {};
 
-  if (!name) newErrors.name = 'Please enter your name.';
-  else if (!validateName(name)) newErrors.name = 'Name must only contain letters.';
+    if (!email) newErrors.email = "Please enter your email.";
+    else if (!validateEmail(email)) newErrors.email = "Invalid email format.";
 
-  if (!pin) newErrors.pin = 'Please enter a four-digit pin.';
-  else if (!validatePin(pin)) newErrors.pin = 'Please enter a valid four-digit pin.';
+    if (!name) newErrors.name = "Please enter your name.";
+    else if (!validateName(name))
+      newErrors.name = "Name must only contain letters.";
 
-  setErrors(newErrors);
-  console.log(newErrors)
-  if (Object.keys(newErrors).length === 0) {
-    try {
-      const requestUrl = `${BASE_API_URL}/users/sign-up`;
-      const requestBody = { email, pin, username: name };
-      console.log(requestBody);
-      const response = await axios.post(requestUrl, requestBody);
-      await AsyncStorage.setItem('userId', response.data.userId);
-      navigation.replace('Bedroom'); 
-    } catch (error) {
-      setErrors({ general: error.message });
+    if (!pin) newErrors.pin = "Please enter a four-digit pin.";
+    else if (!validatePin(pin))
+      newErrors.pin = "Please enter a valid four-digit pin.";
+
+    setErrors(newErrors);
+    console.log(newErrors);
+    if (Object.keys(newErrors).length === 0) {
+      try {
+        const requestUrl = `${BASE_API_URL}/users/sign-up`;
+        const requestBody = { email, pin, username: name };
+        console.log(requestBody);
+        const response = await axios.post(requestUrl, requestBody);
+        await AsyncStorage.setItem("userId", response.data.userId);
+        navigation.replace("Bedroom");
+      } catch (error) {
+        setErrors({ general: error.message });
+      }
     }
-  }
-};
-return (
-    
-  <ScrollView contentContainerStyle={styles.scrollContainer}>
-     <Image source={phyDoodleShapes} style={styles.backgroundImage} />
-    <KeyboardAvoidingView style={styles.container} behavior='padding'>
-    <Text style={styles.header}>Baby University Sign up page</Text>
-      <View style={styles.inputContainer}>
-        <View style={styles.inputRow}>
-          <Text style={styles.label}>Email:</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Enter your Email"
-            value={email}
-            onChangeText={text => setEmail(text)}
-          />
-          {errors.email && <Text style={styles.errorText}>{errors.email}</Text>}
+  };
+  return (
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <View style={styles.titleWrap}>
+          <Text style={styles.titleStroke}>Sign Up</Text>
+          <Text style={styles.title}>Sign Up</Text>
         </View>
-        <View style={styles.inputRow}>
-          <Text style={styles.label}>Name:</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Enter your first name"
-            value={name}
-            onChangeText={text => setName(text)}
-          />
-          {errors.name && <Text style={styles.errorText}>{errors.name}</Text>}
+        <Text style={styles.subtitle}>Create a new account</Text>
+        <View style={styles.inputContainer}>
+          <View style={styles.inputRow}>
+            <View style={styles.labelWrap}>
+              <Text style={styles.label}>Email:</Text>
+            </View>
+            <View style={styles.inputWrap}>
+              <TextInput
+                style={styles.input}
+                placeholder="Enter your Email"
+                value={email}
+                onChangeText={(text) => setEmail(text)}
+              />
+            </View>
+            {errors.email && (
+              <Text style={styles.errorText}>{errors.email}</Text>
+            )}
+          </View>
+          <View style={styles.inputRow}>
+            <View style={styles.labelWrap}>
+              <Text style={styles.label}>Name:</Text>
+            </View>
+            <View style={styles.inputWrap}>
+              <TextInput
+                style={styles.input}
+                placeholder="Enter your first name"
+                value={name}
+                onChangeText={(text) => setName(text)}
+              />
+            </View>
+            {errors.name && <Text style={styles.errorText}>{errors.name}</Text>}
+          </View>
+          <View style={styles.inputRow}>
+            <View style={styles.labelWrap}>
+              <Text style={styles.label}>Pin:</Text>
+            </View>
+            <View style={styles.inputWrap}>
+              <TextInput
+                style={styles.input}
+                placeholder="Enter a four digit pin"
+                secureTextEntry
+                value={pin}
+                onChangeText={(text) => setPin(text)}
+              />
+            </View>
+            {errors.pin && <Text style={styles.errorText}>{errors.pin}</Text>}
+          </View>
         </View>
-        <View style={styles.inputRow}>
-          <Text style={styles.label}>Pin:</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Enter a four digit pin"
-            secureTextEntry
-            value={pin}
-            onChangeText={text => setPin(text)}
-          />
-          {errors.pin && <Text style={styles.errorText}>{errors.pin}</Text>}
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={handleSignup} style={styles.button}>
+            <Text style={styles.buttonText}>Sign Up</Text>
+          </TouchableOpacity>
         </View>
-      </View>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={handleSignup} style={styles.button}>
-          <Text style={styles.buttonText}>Sign Up</Text>
-        </TouchableOpacity>
-      </View>
-      {errors.general && <Text style={styles.errorText}>{errors.general}</Text>}
-      <TouchableOpacity style={styles.goBackButton} onPress={() => navigation.navigate('Login')}>
-<Text style={styles.goBackText}>Go Back</Text>
-</TouchableOpacity>
-
-    </KeyboardAvoidingView>
-  </ScrollView>
-);
+        {errors.general && (
+          <Text style={styles.errorText}>{errors.general}</Text>
+        )}
+        <Image source={intersect} style={styles.backgroundImage} />
+      </KeyboardAvoidingView>
+    </ScrollView>
+  );
 };
 const styles = StyleSheet.create({
   backgroundImage: {
     flex: 1,
-    justifyContent: 'center',
-    height: '100%',
+    justifyContent: "center",
+    height: "100%",
     marginTop: 0,
-    marginBottom: -320,
   },
   header: {
     fontSize: 50,
-    fontFamily: 'Itim_400Regular',
-    color: '#3F3CB4',
+    fontFamily: "Itim_400Regular",
+    color: "#3F3CB4",
     marginVertical: 20,
   },
   scrollContainer: {
     flexGrow: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingVertical: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    // paddingVertical: 20,
     paddingHorizontal: 10,
   },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: "#FFFEFB",
+  },
+  titleStroke: {
+    fontSize: 100,
+    color: "black",
+    position: "absolute",
+    textShadowColor: "black",
+    textShadowOffset: { width: 5, height: 5 },
+    textShadowRadius: 10,
+    fontFamily: "McLaren",
+    fontWeight: "bold",
+  },
+  title: {
+    color: "#B36003",
+    fontFamily: "McLaren",
+    fontSize: 100,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontFamily: "McLaren",
+    fontSize: 30,
   },
   inputContainer: {
-    width: '90%',
-    marginBottom: 20,
+    width: "100%",
+    marginTop: 80,
+    // flexGrow: 1,
+    // flex: 1,
+    // justifyContent: "center",
+    // alignItems: "center",
+    // marginTop: 20,
+  },
+
+  labelWrap: {
+    alignItems: "flex-end",
+    width: 80,
+  },
+  inputWrap: {
+    flexShrink: 1,
+    alignItems: "center",
+    flexDirection: "row",
   },
   inputRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10,
-    justifyContent: 'space-between',
+    width: "fit-content",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    alignContent: "center",
+    marginTop: 20,
+    marginHorizontal: "auto",
+    transform: [{ translateX: -30 }],
   },
   label: {
     marginRight: 10,
-    fontWeight: 'bold',
-    flex: 1,
+    textAlign: "right",
+    fontSize: 20,
+    fontWeight: "bold",
   },
   input: {
-    flex: 2,
-    height: 40,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
+    width: 300,
     paddingHorizontal: 10,
+    paddingVertical: 15,
+    fontSize: 18,
+    backgroundColor: "white",
+    borderWidth: 2,
+    borderColor: "orange",
+    borderRadius: 20,
   },
   buttonContainer: {
-    marginTop: 20,
-    width: '60%',
+    marginTop: 40,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
   },
   button: {
-    backgroundColor: '#3F3CB4',
+    backgroundColor: "#AD620E",
+    flexShrink: 1,
     paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-    alignItems: 'center',
+    paddingHorizontal: 100,
+    borderRadius: 20,
+    alignItems: "center",
   },
   buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "700",
+    fontSize: 20,
   },
   errorText: {
-    color: 'red',
+    color: "red",
     marginTop: 5,
     marginLeft: 10,
     fontSize: 12,
-  },
-  goBackButton: {
-    marginTop: 20,
-  },
-  goBackText: {
-    color: '#3F3CB4',
-    fontSize: 16,
-    fontWeight: 'bold',
   },
 });
 export default SignupScreen;
--- front-end/screens/LoginScreen.js
+++ front-end/screens/LoginScreen.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import * as Font from "expo-font";
 import {
   Text,
   View,
@@ -7,12 +8,14 @@ import {
   StyleSheet,
   KeyboardAvoidingView,
   TouchableOpacity,
+  Image,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import UIBallAnimation from "../ballAnimation/ballAnimation";
 import axios from "axios";
 import { BASE_API_URL } from "@env";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import intersect from "../assets/img/Intersect.png";
 
 const LoginScreen = () => {
   const [email, setEmail] = useState("");
@@ -50,7 +53,7 @@ const LoginScreen = () => {
   };
 
   const handleLogin = () => {
-    if(email.toLocaleLowerCase()=="baby@email.com" && pin=="1234"){
+    if (email.toLocaleLowerCase() == "baby@email.com" && pin == "1234") {
       AsyncStorage.setItem("userId", "670a4e9d54909cf6f8131f7f");
       navigation.navigate("Bedroom");
       return;
@@ -77,28 +80,45 @@ const LoginScreen = () => {
 
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
-      <View style={styles.ballAnimationContainer}>
+      {/* <View style={styles.ballAnimationContainer}>
         <UIBallAnimation />
+      </View> */}
+      <Image source={require("../assets/img/logo.png")} style={styles.logo} />
+      
+      <View style={styles.titleWrap}>
+        <Text style={styles.titleStroke}>Baby University</Text>
+        <Text style={styles.title}>Baby University</Text>
       </View>
+      <Text style={styles.subtitle}>Welcome back!</Text>
       <View style={styles.inputContainer}>
         <View style={styles.inputRow}>
-          <Text style={styles.label}>Email:</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Enter your email"
-            value={email}
-            onChangeText={(text) => setEmail(text)}
-          />
+          <View style={styles.labelWrap}>
+            <Text style={styles.label}>Email:</Text>
+          </View>
+          <View style={styles.inputWrap}>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your email"
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+            />
+          </View>
         </View>
         <View style={styles.inputRow}>
-          <Text style={styles.label}>Pin:</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Enter your pin"
-            secureTextEntry
-            value={pin}
-            onChangeText={(text) => setPin(text)}
-          />
+          <View style={styles.labelWrap}>
+            <Text style={styles.label}>Pin:</Text>
+          </View>
+          <View style={styles.inputWrap}>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your pin"
+              secureTextEntry
+              value={pin}
+              onChangeText={(text) => setPin(text)}
+            />   
+            <Text style={{position:"absolute",right:-120,fontSize:24,color:"#835717",fontWeight:"bold"}}>Forgot Pin</Text>        
+          </View>
+          
         </View>
       </View>
       <View style={styles.buttonContainer}>
@@ -109,10 +129,17 @@ const LoginScreen = () => {
         >
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={handleSignup} style={styles.button}>
+        {/* <TouchableOpacity onPress={handleSignup} style={styles.button}>
           <Text style={styles.buttonText}>Create Account</Text>
+        </TouchableOpacity> */}
+      </View>
+      <View style={styles.signUpWrap}>
+        <Text style={styles.signUpText}>Don't have an account?</Text>
+        <TouchableOpacity onPress={handleSignup}>
+          <Text style={styles.signUpLink}>Sign up</Text>
         </TouchableOpacity>
       </View>
+      <Image source={intersect} style={styles.backgroundImage} />
     </KeyboardAvoidingView>
   );
 };
@@ -122,46 +149,86 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#FFFEFB",
   },
-  ballAnimationContainer: {
+  logo: {
+    marginTop: 30,
+  },
+  titleStroke: {
+    fontSize: 100,
+    color: "black",
     position: "absolute",
-    width: "100%",
-    height: "100%",
-    zIndex: 0,
+    textShadowColor: "black",
+    textShadowOffset: { width: 5, height: 5 },
+    textShadowRadius: 10,
+    fontFamily: "McLaren",
+    fontWeight: "bold",
+  },
+  title: {
+    color: "#B36003",
+    fontFamily: "McLaren",
+    fontSize: 100,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontFamily: "McLaren",
+    fontSize: 30,
   },
   inputContainer: {
-    width: "50%",
-    marginTop: 200,
+    width: "100%",
+    // flexGrow: 1,
+    // flex: 1,
+    // justifyContent: "center",
+    // alignItems: "center",
+    // marginTop: 20,
+  },
+
+  labelWrap: {
+    alignItems: "flex-end",
+    width: 70,
+  },
+  inputWrap: {
+    flexShrink: 1,
+    alignItems: "center",
+    flexDirection: "row",
   },
   inputRow: {
-    width: "100%",
+    width: "fit-content",
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
-    marginTop: 40,
+    alignContent: "center",
+    marginTop: 20,
+    marginHorizontal:"auto",   
+    transform: [{translateX: -30}],
   },
   label: {
-    marginRight: 20,
+    marginRight: 10,
+    textAlign: "right",
+    fontSize: 20,
+    fontWeight: "bold",
   },
   input: {
-    flex: 1,
-    paddingHorizontal: 15,
-    paddingVertical: 20,
-    borderRadius: 15,
-    color: "black",
+    width: 300,
+    paddingHorizontal: 10,
+    paddingVertical: 15,
     fontSize: 18,
     backgroundColor: "white",
+    borderWidth: 2,
+    borderColor: "orange",
+    borderRadius: 20,
   },
   buttonContainer: {
-    width: "30%",
+    width: "100%",
     justifyContent: "center",
     alignItems: "center",
     marginTop: 40,
   },
   button: {
-    backgroundColor: "blue",
-    paddingVertical: 15,
-    paddingHorizontal: 50,
+    backgroundColor: "#AD620E",
+    flexShrink:1,
+    paddingVertical: 10,
+    paddingHorizontal: 100,
     borderRadius: 20,
     alignItems: "center",
     marginBottom: 20,
@@ -171,6 +238,31 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     fontSize: 20,
   },
+  backgroundImage: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: -1,
+  },
+  signUpWrap:{
+    marginTop: 20,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    
+  },
+  signUpText: {
+    color:"835717",
+    fontSize: 20,
+  },
+  signUpLink: {
+    color:"#835717",
+    marginLeft: 10,
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+
 });
 
 export default LoginScreen;
